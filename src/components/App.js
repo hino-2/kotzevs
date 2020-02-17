c58ca377538aa30554b7ba6d684fab061e563f9d
hino-2 <sinigami0@gmail.com>
2020-02-17 16:39:58 +0500

    Menu + background

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React, {PureComponent} from 'react'
-// import ArticleList from './ArticleList'
-// import articles from '../features'
 import CircleMenu from './CircleMenu'
 import 'bootstrap/dist/css/bootstrap.css'
+import './style.css'
 
 class App extends PureComponent {
-  state = {
-    reverted: false
-  }
 
   render() {
     return (
-      <div className="container text-center">
+      <div className="">
+        <div className="container bg-warning" style={{"margin-top": "20px"}}>
+          <h1 style={{"margin-left": "50px"}}>привет я Зевс жми мне на нос</h1>
+        </div>
         <CircleMenu />
       </div>
     )
--- src/components/CircleMenu/index.js
+++ src/components/CircleMenu/index.js
@@ -1,49 +1,84 @@
 import React, {Component} from 'react'
+import './style.css'
 
 export default class CircleMenu extends Component {
+  constructor() {
+    super();
+    this.updatePosition = this.updatePosition.bind(this);
+  }
+
+  calcX = () => { return 410 * window.innerWidth / 1200 }
+  calcY = () => { return 335 * window.innerWidth / 1200 }
+
   state = {
-    isOpen: false
+    isOpen: false,
+    xTog: this.calcX(),
+    yTog: this.calcY(),
+    xMenu: this.calcX() - 28,
+    yMenu: this.calcY() - 28,
+    width: window.innerWidth
   }
 
   toggleExpand = () => {
-    console.log(this.state.isOpen);
     if(!this.state.isOpen){
-      document.getElementById("menu").style.transform="scale(3)";
+      document.getElementById("menu").style.transform="scale(2)";
       document.getElementById("plus").style.transform="rotate(45deg)";
       this.setState({ isOpen: true })
-      console.log('open');
     } else {
       document.getElementById("menu").style.transform="scale(0)";
       document.getElementById("plus").style.transform="rotate(0deg)";
       this.setState({ isOpen: false })
-      console.log('close');
     }
+  }
+
+  updatePosition = () => {
+    if(window.innerWidth != this.state.width) {
+      this.setState({
+        xTog: this.calcX(),
+        yTog: this.calcY(),
+        xMenu: this.calcX() - 28,
+        yMenu: this.calcY() - 28,
+        width: window.innerWidth
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.updatePosition();
+    window.addEventListener("resize", this.updatePosition.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePosition.bind(this));
   }
 
   render () {
     return (
       <div>
-        <div className="toggle" id="toggle" onClick={this.toggleExpand}>
+        <div className="toggle" id="toggle"
+             onClick={this.toggleExpand}
+             style={{"--x": this.state.xTog + "px", "--y": this.state.yTog + "px"}}>
           <i className="fa fa-plus" id="plus"></i>
         </div>
-        <div className="menu" id="menu">
-          <a href="#">
+        <div className="menu" id="menu"
+             style={{"--x": this.state.xMenu + "px", "--y": this.state.yMenu + "px"}}>
+          <a href="http://localhost:3000/" title="Песни">
             <i className="fa fa-microphone"></i>
           </a>
-           <a href="#">
+          <a href="tg://resolve?domain=Zeus_catbot" title="Я в телеграме">
+            <i className="fa fa-telegram"></i>
+          </a>
+          <a href="http://localhost:3000/">
+            <i></i>
+          </a>
+          <a href="http://localhost:3000/" title="Мой хозяин">
             <i className="fa fa-user"></i>
           </a>
-          <a href="#">
-            <i className="fa fa-video-camera"></i>
+          <a href="http://localhost:3000/">
+            <i></i>
           </a>
-          <a href="#">
-            <i className="fa fa-envelope"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-camera"></i>
-          </a>
-          <a href="#">
-            <i className="fa fa-bell"></i>
+          <a href="http://localhost:3000/" title="Фото">
+            <i className="fa fa-camera-retro"></i>
           </a>
         </div>
       </div>
